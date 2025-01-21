4ae20179b0582f171a3f915616d75d7a7e734f5f
Rikelmiv <rikelmiv58@gmail.com>
2025-01-21 12:08:49 -0300

    Program.cs

--- lista_06/Program.cs
+++ lista_06/Program.cs
@@ -7,3 +7,28 @@ nota em categorias: "Excelente" (nota >= 9), "Boa" (7 <= nota < 9), "Regular"
 (5 <= nota < 7) e "Baixa" (nota < 5). Exiba a classificação na tela.
 o Exemplo de Saída: "Excelente", "Boa", "Regular" ou "Baixa".
  */
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um nota: ");
+        int nota = int.Parse(Console.ReadLine());
+        if (nota >= 9)
+        {
+            Console.WriteLine("Parabéns sua nota é EXCELENTE");
+        }
+        else if (nota >= 7)
+        {
+            Console.WriteLine("Parabéns sua nota é BOA");
+        }
+        else if (nota >= 5)
+        {
+            Console.WriteLine("Parabéns sua nota é REGULAR");
+        }
+        else
+        {
+            Console.WriteLine("Infelizmente sua nota está muito baixa");
+        }
+    }
+}
