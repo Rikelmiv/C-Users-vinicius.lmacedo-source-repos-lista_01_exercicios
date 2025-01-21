8148f5b8c2fb9f5983b02f3f71439510e5164084
Rikelmiv <rikelmiv58@gmail.com>
2025-01-21 12:11:16 -0300

    Program.cs

--- lista_10/Program.cs
+++ lista_10/Program.cs
@@ -7,3 +7,22 @@ no valor e mostre o valor final. Caso contrário, exiba o valor original.
 o Exemplo de Saída: "Valor com desconto: [valor final]." ou "Valor original:
 [valor]
  */
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o valor de compra");
+        double produto = double.Parse(Console.ReadLine());
+        if (produto > 100)
+        {
+            double desconto = produto * 0.10;
+            double valorFinal = produto - desconto;
+            Console.WriteLine($"O valor da compra com desconto é: R${valorFinal:F2}");
+        }
+        else
+        {
+            Console.WriteLine($"O valor não possui desconto: R${produto:F2}");
+        }
+    }
+}
