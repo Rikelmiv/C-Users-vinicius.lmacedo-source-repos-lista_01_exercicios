bd79fd0e5c4c57d1260f4970e2b38eb75dc1325e
Rikelmiv <rikelmiv58@gmail.com>
2025-01-21 12:09:30 -0300

    Program.cs

--- lista_07/Program.cs
+++ lista_07/Program.cs
@@ -7,3 +7,28 @@ adequada.
 o Exemplo de Saída: "O primeiro número é maior.", "O segundo número é
 maior." ou "Os números são iguais."
  */
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um numero: ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite um numero: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        if (numero1 > numero2)
+        {
+            Console.WriteLine("O Primeiro numero é maior");
+        }
+        else if (numero1 < numero2)
+        {
+            Console.WriteLine("O segundo numero é maior");
+        }
+        else
+        {
+            Console.WriteLine("Os numeros são iguais");
+        }
+    }
+}
