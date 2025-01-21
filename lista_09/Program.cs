3c4983e35eca52fe72a1d2abb3b2c4541c89c1cf
Rikelmiv <rikelmiv58@gmail.com>
2025-01-21 12:10:43 -0300

    Program.cs

--- lista_09/Program.cs
+++ lista_09/Program.cs
@@ -8,3 +8,44 @@ erro.
 o Exemplo de Saída: "Domingo", "Segunda-feira", etc., ou "Número inválido.
 Digite um número de 1 a 7.
  */
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um numero de 1 a 7: ");
+        int numero = int.Parse(Console.ReadLine());
+        if (numero == 1)
+        {
+            Console.WriteLine("Domingo");
+        }
+        else if (numero == 2)
+        {
+            Console.WriteLine("Segunda - feira");
+        }
+        else if (numero == 3)
+        {
+            Console.WriteLine("Terça - feira");
+        }
+        else if (numero == 4)
+        {
+            Console.WriteLine("Quarta - feira");
+        }
+        else if (numero == 5)
+        {
+            Console.WriteLine("Quinta - feira");
+        }
+        else if (numero == 6)
+        {
+            Console.WriteLine("Sexta - feira");
+        }
+        else if (numero == 7)
+        {
+            Console.WriteLine("Sábado");
+        }
+        else
+        {
+            Console.WriteLine("Número inválido");
+        }
+    }
+}
