765d2b9c35e029fb2d96be982f5885c04ede4662
Rikelmiv <rikelmiv58@gmail.com>
2025-01-21 12:08:20 -0300

    Program.cs

--- lista_05/Program.cs
+++ lista_05/Program.cs
@@ -6,3 +6,20 @@ determine se ela é maior de idade (18 anos ou mais) ou menor de idade.
 Utilize if e else para mostrar a mensagem apropriada.
 o Exemplo de Saída: "Você é maior de idade." ou "Você é menor de idade."
  */
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+        if (idade >= 18)
+        {
+            Console.WriteLine("Você é maior de idade");
+        }
+        else
+        {
+            Console.WriteLine("Você não é maior de idade");
+        }
+    }
+}
