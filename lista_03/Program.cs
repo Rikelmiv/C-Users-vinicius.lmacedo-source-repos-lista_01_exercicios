ce23e8c916f121dad431dd09790540db06f76a86
Rikelmiv <rikelmiv58@gmail.com>
2025-01-21 12:06:34 -0300

    Program.cs

--- lista_03/Program.cs
+++ lista_03/Program.cs
@@ -6,3 +6,20 @@ nota for maior ou igual a 6, o aluno é considerado "Aprovado". Caso
 contrário, ele é "Reprovado". Exiba a situação do aluno na tela.
 o Exemplo de Saída: "Aluno aprovado." ou "Aluno reprovado.
  */
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a nota do aluno: ");
+        int nota = int.Parse(Console.ReadLine());
+        if (nota >= 6)
+        {
+            Console.WriteLine($"O aluno está aprovado");
+        }
+        else
+        {
+            Console.WriteLine("O aluno está reprovado");
+        }
+    }
+}
